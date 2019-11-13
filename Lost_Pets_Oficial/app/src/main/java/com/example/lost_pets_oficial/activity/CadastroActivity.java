a772c73c452465249169cf9b677dac38cd992f00
ProcG <gsilvaproc@gmail.com>
2019-11-13 21:21:08 -0200

    Atualiza cadastro sem firebase

--- Lost_Pets_Oficial/app/src/main/java/com/example/lost_pets_oficial/activity/CadastroActivity.java
+++ Lost_Pets_Oficial/app/src/main/java/com/example/lost_pets_oficial/activity/CadastroActivity.java
@@ -1,101 +1,52 @@
 package com.example.lost_pets_oficial.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import com.example.lost_pets_oficial.R;
-import com.example.lost_pets_oficial.config.ConfiguracaoFireBase;
-import com.example.lost_pets_oficial.model.Usuario;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
-public class CadastroActivity extends AppCompatActivity {
+    public class CadastroActivity extends IntroActivity {
 
-    private EditText campoNome, campoEmail, campoSenha;
-    private Button botaoCadastrar;
-    private FirebaseAuth autenticacao;
-    private Usuario usuario;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+            setButtonNextVisible(false);
+            setButtonBackVisible(false);
 
-        campoNome = findViewById(R.id.editNome);
-        campoEmail = findViewById(R.id.editEmail);
-        campoSenha = findViewById(R.id.editSenha);
-        botaoCadastrar = findViewById(R.id.btCadastrar);
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro1)
+                    .build()
+            );
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro3)
+                    .build()
+            );
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro2)
+                    .build()
+            );
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro4)
+                    .build()
+            );
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro5)
+                    .build()
+            );
+            addSlide(new FragmentSlide.Builder()
+                    .background(android.R.color.white)
+                    .fragment(R.layout.cad_intro6)
+                    .canGoForward(false)
+                    .build()
 
-
-        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String textoNome = campoNome.getText().toString();
-                String textoEmail = campoEmail.getText().toString();
-                String textoSenha = campoSenha.getText().toString();
-
-                if (textoNome.isEmpty()){
-                    if (!textoEmail.isEmpty()){
-                        if (!textoSenha.isEmpty()){
-                        usuario = new Usuario();
-                        usuario.setNome(textoNome);
-                        usuario.setEmail(textoEmail);
-                        usuario.setSenha(textoSenha);
-                        cadastrarUsuario();
-
-                        }else{
-                            Toast.makeText(CadastroActivity.this,
-                                    "Preencha a Senha",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                }else {
-                        Toast.makeText(CadastroActivity.this,
-                                "Preencha o Email",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(CadastroActivity.this,
-                            "Preencha o Nome",
-                            Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
+            );
+        }
     }
 
-    public void  cadastrarUsuario(){
-
-        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
-        autenticacao.createUserWithEmailAndPassword(
-                usuario.getEmail(), usuario.getSenha()
-        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-
-                    Toast.makeText(CadastroActivity.this,
-                            "Sucesso ao cadastrar usuário",
-                            Toast.LENGTH_SHORT).show();
-
-                }else{
-
-
-                    Toast.makeText(CadastroActivity.this,
-                            "Erro ao cadastrar usuário",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-    }
-}
